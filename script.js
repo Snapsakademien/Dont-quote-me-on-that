9ef2b6eba8eb4334907dff8b1d7f7707a4f0f796
Tobias Skarhed <1438972+tskarhed@users.noreply.github.com>
2020-04-01 21:01:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -36,9 +36,9 @@ firebase.auth().onAuthStateChanged(function(user) {
 
   function renderQuotes(quotes){
     var wrapper = document.querySelector("#quotes");
-    var quoteHTML = quotes.map(function(quote){
-        return `<div class="citat-box">
-        <div class="citat">${quote.quote}</div>
+    var quoteHTML = quotes.map(function(quote, i){
+        return `<div class="citat-box" id="${i}">
+        <div class="citat"><a class="link" href="#${i}">#${i}</a>${quote.quote}</div>
         <div class="person">
             <i>-${quote.who}, ${new Intl.DateTimeFormat("sv-SE").format(quote.datetime.toDate())}</i>
         </div>
